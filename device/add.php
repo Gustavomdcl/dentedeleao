68fbe1ea4ba72b2670dc5ec1a2df43b42084af72
rafaelrigoni <me@rafaelrigoni.com>
2014-05-03 12:57:03 -0300

    Update add.php

--- device/add.php
+++ device/add.php
@@ -1,7 +1,7 @@
 <?php
    include("conec.php");
    $link=Conection();
-$Sql="insert into DL_DEVICE (device,umi,umisolo,temp,chuva,data)  values ('".$_GET["device"]."','".$_GET["umi"]."','".$_GET["umisolo"]."','".$_GET["temp"]."','".$_GET["chuva"]."', NOW())";      
+$Sql="insert into DL_DEVICE_1001 (dispositivo,umidade,umidade_do_solo,temperatura,chuva,data)  values ('".$_GET["dispositivo"]."','".$_GET["umidade"]."','".$_GET["umidade_do_solo"]."','".$_GET["temperatura"]."','".$_GET["chuva"]."', NOW())";      
    mysql_query($Sql,$link); 
    header("Location: insertareg.php");
 ?>
