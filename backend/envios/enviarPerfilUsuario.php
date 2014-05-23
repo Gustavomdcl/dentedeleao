a719cf09f4e370cd217b55e07bf5e0b6ebc7d716
Gustavo Lima <gustavomdcl@gmail.com>
2014-05-23 20:01:16 -0300

    vIvideo

--- backend/envios/enviarPerfilUsuario.php
+++ backend/envios/enviarPerfilUsuario.php
@@ -39,69 +39,73 @@
 		$plantacao = $plantacao . '/' . $value;
 	}
 
-	$demaisplantacoes 	=	$_POST['demaisplantacoes'];
-	if ($_POST['demaisplantacoes'] != null && $_POST['demaisplantacoes'] != 'null') {
-		function multiexplode ($delimiters,$string) {
-		    $ready = str_replace($delimiters, $delimiters[0], $string);
-		    $launch = explode($delimiters[0], $ready);
-		    return  $launch;
+	if($usuarioId==null){
+		echo '<script>window.location.assign("../../index.php");</script>';
+	} else {
+		$demaisplantacoes 	=	$_POST['demaisplantacoes'];
+		if ($_POST['demaisplantacoes'] != null && $_POST['demaisplantacoes'] != 'null') {
+			function multiexplode ($delimiters,$string) {
+			    $ready = str_replace($delimiters, $delimiters[0], $string);
+			    $launch = explode($delimiters[0], $ready);
+			    return  $launch;
+			}
+			$condition = array(',' , ', ' , ' , ' , ' ,');
+			$plantacoesValidar = multiexplode($condition, $demaisplantacoes);
+			foreach ($plantacoesValidar as $values){
+
+				$sqlPlantacaoValidar = "INSERT INTO DL_ADMIN_plantationList(plantacao, valido) VALUES ('$values', '0')";
+				mysqlexecuta($id, $sqlPlantacaoValidar);
+				$plantacao = $plantacao . '/' . mysql_insert_id();
+			}
 		}
-		$condition = array(',' , ', ' , ' , ' , ' ,');
-		$plantacoesValidar = multiexplode($condition, $demaisplantacoes);
-		foreach ($plantacoesValidar as $values){
 
-			$sqlPlantacaoValidar = "INSERT INTO DL_ADMIN_plantationList(plantacao, valido) VALUES ('$values', '0')";
-			mysqlexecuta($id, $sqlPlantacaoValidar);
-			$plantacao = $plantacao . '/' . mysql_insert_id();
-		}
-	}
+		// IMAGENS ==========
 
-	// IMAGENS ==========
+		if($nome_img[0]==null){} else {//Se imagem foi enviada
+		
+			for($i = 0; $i < $qtd; $i++){
 
-	if($nome_img[0]==null){} else {//Se imagem foi enviada
-	
-		for($i = 0; $i < $qtd; $i++){
+				//SE NOME EXISTE =========
 
-			//SE NOME EXISTE =========
+				$nameimg = explode(".", $nome_img[$i]);
 
-			$nameimg = explode(".", $nome_img[$i]);
+				$increment = ''; //start with no suffix
 
-			$increment = ''; //start with no suffix
+				$nome_imgFinal = '';
 
-			$nome_imgFinal = '';
+				for($j = 0; $j < (count($nameimg)-1); $j++){
+					$nome_imgFinal = $nome_imgFinal . $nameimg[$j];
+				}
 
-			for($j = 0; $j < (count($nameimg)-1); $j++){
-				$nome_imgFinal = $nome_imgFinal . $nameimg[$j];
+				while(file_exists($uploadDir . $nome_imgFinal . $increment . '.' . end($nameimg))) {
+				    $increment++;
+				}
+
+				$nome_img[$i] = $nome_imgFinal . $increment . '.' . end($nameimg);
+
+				//SOBE NO SITE ===========
+
+				move_uploaded_file($tmps[$i], $uploadDir.$nome_img[$i]);
+				$insertimg = mysql_query("INSERT INTO DL_IMAGES(caminho, nome_imagem) VALUES('$dir', '$nome_img[$i]')");
+				$resultedimg = mysql_query($insertimg);	
+				/*echo 'Imagem enviada com sucesso';
+				echo '<br><img src="'.$uploadDir.$nome_img[$i].'">';
+				echo '<br>';*/
+
+				//O ID DA IMAGEM QUE SUBIU PARA SER USADO NO PRÓXIMO INSERT
+				$imagemTabelaId = $imagemTabelaId . mysql_insert_id().'-';
 			}
 
-			while(file_exists($uploadDir . $nome_imgFinal . $increment . '.' . end($nameimg))) {
-			    $increment++;
-			}
-
-			$nome_img[$i] = $nome_imgFinal . $increment . '.' . end($nameimg);
-
-			//SOBE NO SITE ===========
-
-			move_uploaded_file($tmps[$i], $uploadDir.$nome_img[$i]);
-			$insertimg = mysql_query("INSERT INTO DL_IMAGES(caminho, nome_imagem) VALUES('$dir', '$nome_img[$i]')");
-			$resultedimg = mysql_query($insertimg);	
-			/*echo 'Imagem enviada com sucesso';
-			echo '<br><img src="'.$uploadDir.$nome_img[$i].'">';
-			echo '<br>';*/
-
-			//O ID DA IMAGEM QUE SUBIU PARA SER USADO NO PRÓXIMO INSERT
-			$imagemTabelaId = $imagemTabelaId . mysql_insert_id().'-';
 		}
 
+		// ENVIO ==========
+
+		$sqlFinal = "INSERT INTO DL_PROFILE(usuario, nome, foto, cpf, email, telefone, celular, fazenda, cnpj, endereco, latitude, longitude, cep, estado, cidade, plantacoes) VALUES('$usuarioId', '$nome', '$imagemTabelaId', '$cpf', '$email', '$telefone', '$celular', '$nomefazenda', '$cnpjfazenda', '$enderecofazenda', '$lat', '$long', '$cepfazenda', '$estado', '$cidade', '$plantacao')";
+		mysqlexecuta($id, $sqlFinal);
+
+		$mensagemValidacao = 'primeiro-perfil-atualizado';
+
+		echo '<script>window.location.assign("../../painel.php?sucesso=' . $mensagemValidacao . '");</script>';
 	}
-
-	// ENVIO ==========
-
-	$sqlFinal = "INSERT INTO DL_PROFILE(usuario, nome, foto, cpf, email, telefone, celular, fazenda, cnpj, endereco, latitude, longitude, cep, estado, cidade, plantacoes) VALUES('$usuarioId', '$nome', '$imagemTabelaId', '$cpf', '$email', '$telefone', '$celular', '$nomefazenda', '$cnpjfazenda', '$enderecofazenda', '$lat', '$long', '$cepfazenda', '$estado', '$cidade', '$plantacao')";
-	mysqlexecuta($id, $sqlFinal);
-
-	$mensagemValidacao = 'primeiro-perfil-atualizado';
-
-	echo '<script>window.location.assign("../../painel.php?sucesso=' . $mensagemValidacao . '");</script>';
 
 ?>
